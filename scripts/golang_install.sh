63eaf354004dc5036fe5e49dd5509516875a2b60
autuanliu <autuanliu@163.com>
2018-01-09 21:14:39 +0800

    golang install

--- scripts/golang_install.sh
+++ scripts/golang_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # golang install
 # autuanliu@163.com
+# https://golang.org/doc/install?download=go1.9.2.linux-amd64.tar.gz
 
-
+curl -O https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+rm go1.9.2.linux-amd64.tar.gz
