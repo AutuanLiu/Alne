a22eeb2278c149c0deabf92cd0f099c8ac9c6e58
autuanliu <autuanliu@163.com>
2018-01-09 21:15:45 +0800

    fix:orange:

--- scripts/golang_install.sh
+++ scripts/golang_install.sh
@@ -6,4 +6,5 @@
 curl -O https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+echo "PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 rm go1.9.2.linux-amd64.tar.gz
