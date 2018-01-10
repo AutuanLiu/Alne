5225a1ba7513570b4181c13d341635f93aff2ca8
autuanliu <autuanliu@163.com>
2018-01-10 17:49:28 +0800

    update

--- scripts/matlab.sh
+++ scripts/matlab.sh
@@ -3,10 +3,13 @@
 # autuanliu@163.com
 
 # download
-# dvd1
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part1.rar
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part2.rar
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part3.rar
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part4.rar
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part5.rar
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016b_Linux_Crack_Only_Downloadly.ir.rar
+
+# other place
+# http://ftp.math.ncu.edu.tw/download/2017a/linux.zip
+# http://matlab.math.ncu.edu.tw/download.html
--- scripts/wechat_install.sh
+++ scripts/wechat_install.sh
@@ -5,10 +5,15 @@
 
 # Clone this repository
 git clone https://github.com/geeeeeeeeek/electronic-wechat.git
+
 # Go into the repository
 cd electronic-wechat
+
 # Install dependencies and run the app
 npm install && npm start
 
 # To pack into an app, simply type one of these:
 npm run build:linux
+
+# remove
+rm -rf electronic-wechat
