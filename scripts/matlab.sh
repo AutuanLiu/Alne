babec4ae3b898af942d468641b496b3080aef4e7
autuanliu <autuanliu@163.com>
2018-01-26 15:28:01 +0800

    update

--- scripts/matlab.sh
+++ scripts/matlab.sh
@@ -3,6 +3,7 @@
 # autuanliu@163.com
 
 # download
+
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part1.rar
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part2.rar
 curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016_Linux_Downloadly.ir.part3.rar
@@ -14,3 +15,4 @@ curl -O http://dl.downloadly.ir/Files/Software/Mathworks_Matlab_R2016b_Linux_Cra
 # http://ftp.math.ncu.edu.tw/download/2017a/linux.zip
 # http://matlab.math.ncu.edu.tw/download.html
 # https://downloadly.ir/
+# 2016版成功安装2017没有尝试
